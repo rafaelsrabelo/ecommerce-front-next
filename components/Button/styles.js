84961cbe00fbec6d1a83bc6f73bccaae98cc4c19
Rafael Rabelo <rafaelrabelo1610@gmail.com>
2023-05-24 11:28:36 -0300

    style(Button): improve button style by adding hover effect and changing padding and colors feat(Button): add cart icon to the secondary button feat(Home): change page title and remove unnecessary text from the description

--- components/Button/styles.js
+++ components/Button/styles.js
@@ -2,15 +2,26 @@ const { styled } = require("styled-components");
 
 
 export const ButtonStyle = styled.button`
-    background: ${props => props.secondary ? 'blue' : 'red'};
-    
+    display: flex;
+    align-items: center;
+    gap: 2px;
+    background: ${props => props.secondary ? '#483BEE' : '#c3c3c3'};
+    color: ${props =>  props.secondary ? props.theme.white : props.theme.black};
     border:0;
-    padding: 5px 15px;
+    padding: 10px 15px;
     border-radius: 5px;
     cursor: pointer;
     display: inline-flex;
     align-items: center;
     text-decoration: none;
-    /* font-family: 'Poppins', sans-serif; */
     font-weight:500;
+
+    transition: 0.8s;
+    svg {
+        height: 24px;
+        width: 24px;
+    }
+    &:hover {
+        filter: brightness(0.9);
+    }
 `
